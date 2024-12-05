796827cbdcd77e9c0dfdb5b045056be8b5307aa3
Anders Kjeldsen <anders@appify.no>
2024-12-05 17:30:44 +0100

    added linux

---
+++ src/native-c/linux-x64/Am/Fipm/SinusTable.c
@@ -0,0 +1,11 @@
+#include <libc/core.h>
+#include <Am/Fipm/SinusTable.h>
+#include <linux-x64/Am/Fipm/SinusTable.h>
+#include <Am/Lang/ClassRef.h>
+#include <Am/Lang/Object.h>
+#include <Am/Lang/Int.h>
+#include <Am/Lang/Array.h>
+#include <Am/Lang/Exception.h>
+#include <Am/Lang/String.h>
+#include <libc/core_inline_functions.h>
+
